0a505794a16fa84988cf1b92b624dd1265e00e33
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-04-05 13:17:21 +0100

    Cm/kibana (#4)
    * Add docker-compose.yml for Kibana and Elasticsearch
    
    * Add pino-elasticsearch and ts-node to package.json
    
    * Add pino-elasticsearch for logging to Elasticsearch
    
    * Add README.md for setting up Kibana and Elasticsearch in Docker

--- packages/pino/src/logger.ts
+++ packages/pino/src/logger.ts
@@ -1,5 +1,15 @@
 import pino from 'pino';
+import pinoElasticsearch from 'pino-elasticsearch';
 
-export const logger = pino({
-  level: 'info',
+const streamToElastic = pinoElasticsearch({
+  index: 'logs',
+  type: 'log',
+  node: 'http://localhost:9200',
 });
+
+export const logger = pino(
+  {
+    level: 'info',
+  },
+  streamToElastic
+);
