d3eaa58454858d8e42ebf0017d32ae152909cdbc
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-08 21:38:04 +0100

    Add streamToUint8Array utility function

---
+++ packages/lambdas/src/utils/streamToUint8Array.ts
@@ -0,0 +1,10 @@
+export const streamToUint8Array = async (
+  stream: NodeJS.ReadableStream
+): Promise<Uint8Array> => {
+  return new Promise((resolve, reject) => {
+    const chunks: Uint8Array[] = [];
+    stream.on('data', (chunk: Uint8Array) => chunks.push(chunk));
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+    stream.on('error', reject);
+  });
+};
