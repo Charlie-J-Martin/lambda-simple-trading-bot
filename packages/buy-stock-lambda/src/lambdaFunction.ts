94483eac055db35d3049f58d7c7ac77c1f4c68de
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-04-09 10:10:19 +0100

    Cm/create buy lambda function (#8)
    * Add buy-stock-lambda package and createBuyLambda function
    
    * Refactor lambda functions and remove unused code

--- packages/buy-stock-lambda/src/lambdaFunction.ts
+++ packages/buy-stock-lambda/src/lambdaFunction.ts
@@ -1,8 +1,5 @@
 import { Handler } from 'aws-lambda';
 
-// event is any data that is passed to the function when it is invoked
-// context provides information about the invocation, function, and execution environment
-
 export const handler: Handler = async (event, context) => {
   console.log(
     'Embrace challenges with courage and persistence. Each obstacle you overcome is a step forward towards your goals. Believe in yourself and keep striving for greatness.: \n' +
