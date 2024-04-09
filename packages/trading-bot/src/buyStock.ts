e93dfceaa12ee9af456ba78c17f3003d56e7668b
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-09 18:09:03 +0100

    Refactor buyStock function to use triggerLambda for buying stocks and update logging

--- packages/trading-bot/src/buyStock.ts
+++ packages/trading-bot/src/buyStock.ts
@@ -1,11 +1,39 @@
+import { lambdaClient } from '../../lambda-client/src/lambdaClient';
 import { convertToLowestDenomination } from '../../utils/src/convertToLowestDenomination';
-import { logger } from '../../pino/src/logger';
+import { InvokeCommand } from '@aws-sdk/client-lambda';
 
-export const buyStock = (cash: number, price: number) => {
+export const buyStock = async (cash: number, price: number) => {
   const convertedPrice = convertToLowestDenomination(price);
-  const numberOfStocks = Number((cash / convertedPrice).toFixed(5));
-  const totalCost = numberOfStocks * convertedPrice;
-  cash = Number((cash - totalCost).toFixed(0));
-  logger.info(`Buying ${numberOfStocks} stocks at ${price}`);
-  return [numberOfStocks, cash];
+  const response = await triggerLambda(cash, convertedPrice);
+  const { numberOfStocks, remainingCash } = JSON.parse(response.body);
+  console.log(
+    `Bought ${numberOfStocks} stocks at $${price} each, remaining cash: $${remainingCash}`
+  );
+  return [numberOfStocks, remainingCash];
 };
+
+// Define the function to trigger the Lambda
+export async function triggerLambda(cash: number, price: number) {
+  try {
+    const eventPayload = {
+      cash,
+      price,
+    };
+    // Create an instance of the InvokeCommand
+    const command = new InvokeCommand({
+      FunctionName: 'BuyStockLambdaFunction', // Name of your Lambda function
+      Payload: JSON.stringify(eventPayload), // Event payload (empty for this example)
+    });
+
+    const response = await lambdaClient.send(command);
+    // convert payload from unit8array to string
+    const payload = response.Payload as Uint8Array;
+    const decodedPayload = JSON.parse(Buffer.from(payload).toString());
+    console.log({ decodedPayload });
+    return decodedPayload;
+  } catch (err) {
+    console.error('Error triggering Lambda:', err);
+  }
+}
+
+
