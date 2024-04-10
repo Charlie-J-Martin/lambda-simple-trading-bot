2303fb3d48d18c8ddbebca2f784f4442942046b0
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-04-10 13:26:14 +0100

    Refactor Jest configuration and add unit tests for sell-stock-lambda package (#13)

--- packages/sell-stock-lambda/src/lambdaFunction.ts
+++ packages/sell-stock-lambda/src/lambdaFunction.ts
@@ -1,9 +1,36 @@
 import { Handler } from 'aws-lambda';
 
-export const handler: Handler = async (event, context) => {
-  console.log(
-    'Embrace challenges with courage and persistence. Each obstacle you overcome is a step forward towards your goals. Believe in yourself and keep striving for greatness.: \n' +
-      JSON.stringify(event, null, 2)
-  );
-  return context.logStreamName;
+export const handler: Handler = async (event, _context) => {
+  try {
+    if (
+      !event.numberOfStocks ||
+      !event.price ||
+      typeof event.numberOfStocks !== 'number' ||
+      typeof event.price !== 'number'
+    ) {
+      throw new Error(
+        'Invalid input: numberOfStocks and price must be numbers'
+      );
+    }
+
+    const { numberOfStocks, price } = event;
+    const totalSale = Number((numberOfStocks * price).toFixed(5));
+    const cash = Number(totalSale.toFixed(0));
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ numberOfStocks: 0, cash }),
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'An error occurred',
+          error: error.message,
+          event: event,
+        }),
+      };
+    }
+  }
 };
