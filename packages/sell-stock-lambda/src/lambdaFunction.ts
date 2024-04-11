048376b811c3665a05b50df7980184b17eb48be2
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-11 06:37:42 +0100

    Refactor input validation in sell-stock-lambda

--- packages/sell-stock-lambda/src/lambdaFunction.ts
+++ packages/sell-stock-lambda/src/lambdaFunction.ts
@@ -3,22 +3,20 @@ import { Handler } from 'aws-lambda';
 export const handler: Handler = async (event, _context) => {
   try {
     if (
-      !event.numberOfStocks ||
+      !event.stocks ||
       !event.price ||
-      typeof event.numberOfStocks !== 'number' ||
+      typeof event.stocks !== 'number' ||
       typeof event.price !== 'number'
     ) {
-      throw new Error(
-        'Invalid input: numberOfStocks and price must be numbers'
-      );
+      throw new Error('Invalid input: stocks and price must be numbers');
     }
 
-    const { numberOfStocks, price } = event;
-    const totalSale = Number((numberOfStocks * price).toFixed(5));
+    const { stocks, price } = event;
+    const totalSale = Number((stocks * price).toFixed(5));
     const cash = Number(totalSale.toFixed(0));
     return {
       statusCode: 200,
-      body: JSON.stringify({ numberOfStocks: 0, cash }),
+      body: JSON.stringify({ stocks: 0, cash }),
     };
   } catch (error) {
     if (error instanceof Error) {
