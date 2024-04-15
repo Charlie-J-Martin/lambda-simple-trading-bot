84a87cae950c321dd0bfd09116f23a2a5c8cf437
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-15 18:21:01 +0100

    Refactor frontend code and update file extensions

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import './index.css';
 import './App.css';
-import Clock from './components/clock/clock';
+import SimulationDayDisplay from './components/SimulationDayDisplay/SimulationDayDisplay';
 import StockInformation from './components/StockInformation/StockInformation';
 
 const App = () => {
   return (
     <>
-      {/* <h1 className='text-4xl text-center mt-4'>Hello, Tailwind CSS!</h1> */}
-      {/* <p className='text-center mt-4'>This is a simple Tailwind CSS example.</p> */}
-
       <div className='app-container'>
         <div className='container mx-auto'>
           <div className='grid grid-cols-5 gap-2'>
-            <div className='bg-yellow-100 p-4'>
-              <Clock />
+            <div className='p-4'></div>
+            <div className='col-span-3 p-4 text-center text-white'>
+              <h1 className='text-4xl text-center mt-4'>Bot Trade Central</h1>
             </div>
-            <div className='bg-yellow-200 p-4'>02</div>
-            <div className='bg-yellow-300 p-4'>03</div>
-            <div className='bg-yellow-300 p-4'>03</div>
-            <div className='bg-yellow-300 p-4'>03</div>
-            {/* <div className='bg-yellow-500 p-4'>05</div> */}
-            <div className='bg-yellow-900 p-4'>
+            <div className='p-4'></div>
+            <div>
+              <SimulationDayDisplay />
+            </div>
+            <div className='col-span-4'></div>
+            <div className='bg-yellow-100 p-4'>
               <StockInformation />
             </div>
             <div className='bg-yellow-100 p-4'>10</div>
             <div className='bg-yellow-200 p-4'>11</div>
             <div className='bg-yellow-300 p-4'>12</div>
-            <div className='bg-yellow-300 p-4'>03</div>
+            <div className='bg-yellow-300 p-4'>13</div>
             <div className='col-span-5 bg-yellow-600 p-4'>STOCK TABles</div>
           </div>
         </div>
