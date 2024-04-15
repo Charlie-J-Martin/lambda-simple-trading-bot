447e8fc1007f950d4ba5cfb1c953f826d9d5028a
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-15 10:44:19 +0100

    Refactor frontend code and update file extensions

---
+++ packages/frontend/src/components/StockInformation/StockInformation.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './StockInformation.css';
+
+const StockInformation = () => {
+  return <div className='stockinformation-container'>StockInformation</div>;
+};
+
+export default StockInformation;
