35573b8857731e30d8edf0daed871faa696047eb
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-16 19:58:19 +0100

    Refactor socket event handlers to include day parameter and update delay time

--- packages/frontend/src/components/StockInformation/StockInformation.jsx
+++ packages/frontend/src/components/StockInformation/StockInformation.jsx
@@ -1,53 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import './StockInformation.css';
-import useSocket from '../../useSocket';
+import socketIOClient from 'socket.io-client';
 import Prices from '../Prices/Prices';
 import MetaData from '../MetaData/MetaData';
 
-const StockInformation = ({ ticker }) => {
-  const [previousData, setPreviousData] = useState(null);
-  const stockData = useSocket('http://localhost:3001', 'AAPL');
+const StockInformation = () => {
+  const [stockData, setData] = useState({
+    o: 0,
+    h: 0,
+    l: 0,
+    c: 0,
+    vw: 0,
+    v: 0,
+  });
 
   useEffect(() => {
-    setPreviousData(stockData.data);
-  }, [stockData, previousData]);
+    const socket = socketIOClient('http://localhost:3001');
+
+    socket.on('AAPL', (data) => {
+      setData(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <div className='stock-information-container'>
       <h1 className='text-4xl text-center text-black'>APPL</h1>
       <div className='grid grid-cols-2'>
         <div className='p-1'>
-          <Prices
-            priceName={'Open'}
-            priceValue={stockData.data.o}
-            color={'green'}
-          />
+          <Prices priceName={'Open'} priceValue={stockData.o} color={'green'} />
         </div>
         <div className='p-1'>
-          <Prices
-            priceName={'Close'}
-            priceValue={stockData.data.c}
-            color={'red'}
-          />
+          <Prices priceName={'Close'} priceValue={stockData.c} color={'red'} />
         </div>
       </div>
       <div className='grid grid-cols-3'>
         <div className='p-1'>
-          <MetaData dataPointName={'High'} dataPointValue={stockData.data.h} />
+          <MetaData dataPointName={'High'} dataPointValue={stockData.h} />
         </div>
         <div className='p-1'>
-          <MetaData dataPointName={'Low'} dataPointValue={stockData.data.l} />
+          <MetaData dataPointName={'Low'} dataPointValue={stockData.l} />
         </div>
         <div className='p-1'>
           <MetaData
             dataPointName={'Avg. Price'}
-            dataPointValue={stockData.data.vw}
+            dataPointValue={stockData.vw}
           />
         </div>
         <div className='col-span-3 p-1'>
           <MetaData
             dataPointName={'Volume Traded'}
-            dataPointValue={stockData.data.v}
+            dataPointValue={stockData.v}
           />
         </div>
       </div>
