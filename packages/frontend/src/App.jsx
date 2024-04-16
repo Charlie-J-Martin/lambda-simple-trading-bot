f9dab54a5bd42a687d5bc0dbbf1729b361d871a3
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-04-16 10:47:18 +0100

    Cm/initial stock info display (#21)
    * Add Google Fonts stylesheet to index.html
    
    * Refactor font-family in CSS files
    
    * Refactor Prices component and add StockInformation component
    
    * Refactor grid layout in App.jsx
    
    * Refactor SimulationDayDisplay component CSS

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -9,24 +9,21 @@ const App = () => {
     <>
       <div className='app-container'>
         <div className='container mx-auto'>
-          <div className='grid grid-cols-5 gap-2'>
+          <div className='grid grid-cols-3 gap-2'>
             <div className='p-4'></div>
-            <div className='col-span-3 p-4 text-center text-white'>
+            <div className='col-span-1 p-4 text-center text-white'>
               <h1 className='text-4xl text-center mt-4'>Bot Trade Central</h1>
             </div>
             <div className='p-4'></div>
             <div>
               <SimulationDayDisplay />
             </div>
-            <div className='col-span-4'></div>
-            <div className='bg-yellow-100 p-4'>
+            <div className='col-span-3 '></div>
+            <div className='flex justify-center'>
               <StockInformation />
             </div>
-            <div className='bg-yellow-100 p-4'>10</div>
-            <div className='bg-yellow-200 p-4'>11</div>
-            <div className='bg-yellow-300 p-4'>12</div>
-            <div className='bg-yellow-300 p-4'>13</div>
-            <div className='col-span-5 bg-yellow-600 p-4'>STOCK TABles</div>
+            <div className='bg-yellow-100 p-4'></div>
+            <div className='bg-yellow-200 p-4'></div>
           </div>
         </div>
       </div>
