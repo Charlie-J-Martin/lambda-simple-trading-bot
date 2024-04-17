462efb5bf01dec921066bf83501602b7b2b7d934
Charlie Martin <56304371+Charlie-J-Martin@users.noreply.github.com>
2024-04-17 10:32:38 +0100

    Cm/hook up trading bot stats (#25)
    * Refactor stockDecisionMaker to remove unused code and improve readability
    
    * Add socket event handler for 'statistics' in socket.ts and collect statistics in statistics.ts
    
    * Add TradingBot component and its CSS file
    
    * Refactor grid layout in App.jsx to improve readability and remove unused CSS classes
    
    * Refactor StockInformation component layout and styles
    
    * Update color in MetaData.css and Prices.css to use rgb(0, 0, 0)

--- packages/frontend/src/components/TradingBot/TradingBot.jsx
+++ packages/frontend/src/components/TradingBot/TradingBot.jsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+import './TradingBot.css';
 
 const TradingBot = () => {
+  const [statistics, setStatistics] = useState({
+    currentCash: 0,
+    currentStocks: 0,
+    buyTrades: 0,
+    sellTrades: 0,
+    holdTrades: 0,
+    buyTradesPercentage: 0,
+    sellTradesPercentage: 0,
+    holdTradesPercentage: 0,
+    totalProfitLoss: 0,
+    totalProfitLossPercentage: 0,
+    totalTrades: 0,
+  });
+
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:3001');
+
+    socket.on('statistics', (data) => {
+      setStatistics(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  console.log({ stats: statistics });
   return (
     <>
-      <div>TradingBot</div>
+      <h1 className='text-3xl text-center'>Trading Bot</h1>
+
+      <div className='trading-bot-container'>
+        <div className='grid grid-cols-2 gap-2'>
+          <div className='p-2'>
+            <h1 className='text-xl text-center'>
+              Cash Held: ${statistics.currentCash}
+            </h1>
+          </div>
+          <div className='p-2'>
+            <h1 className='text-xl text-center'>
+              Stocks Held: {statistics.currentStocks}
+            </h1>
+          </div>
+        </div>
+        <div>
+          <div className='p-2'>
+            <h1 className='text-center'>
+              Buy Calls: {statistics.buyTrades} |{' '}
+              {statistics.buyTradesPercentage}%
+            </h1>
+          </div>
+          <div className='p-2'>
+            <h1 className='text-center'>
+              Sell Calls: {statistics.sellTrades} |{' '}
+              {statistics.sellTradesPercentage}%
+            </h1>
+          </div>
+          <div className='p-2'>
+            <h1 className='text-center'>
+              Hold Calls: {statistics.holdTrades} |{' '}
+              {statistics.sellTradesPercentage}%
+            </h1>
+          </div>
+          <div className='p-2'>
+            <h1 className='text-center'>
+              Profit / Loss: ${statistics.totalProfitLoss} |{' '}
+              {statistics.totalProfitLossPercentage}%
+            </h1>
+          </div>
+          <div className='p-2'>
+            <h1 className='text-center'>
+              Total Trades: {statistics.totalTrades}
+            </h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
