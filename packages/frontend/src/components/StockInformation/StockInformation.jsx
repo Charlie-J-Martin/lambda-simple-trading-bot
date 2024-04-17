3542aa1bf73d9b17dd20db97b0f4fe4aa6dc4f0b
Charlie-J-Martin <charliejm7@gmail.com>
2024-04-17 10:31:01 +0100

    Refactor StockInformation component layout and styles

--- packages/frontend/src/components/StockInformation/StockInformation.jsx
+++ packages/frontend/src/components/StockInformation/StockInformation.jsx
@@ -27,37 +27,48 @@ const StockInformation = () => {
   }, []);
 
   return (
-    <div className='stock-information-container'>
-      <h1 className='text-4xl text-center text-black'>APPL</h1>
-      <div className='grid grid-cols-2'>
-        <div className='p-1'>
-          <Prices priceName={'Open'} priceValue={stockData.o} color={'green'} />
+    <>
+      <h1 className='text-3xl text-center'>Ticker Information</h1>
+      <div className='stock-information-container'>
+        <h1 className='text-2xl text-center text-black'>APPL</h1>
+        <div className='grid grid-cols-2'>
+          <div className='p-1'>
+            <Prices
+              priceName={'Open'}
+              priceValue={stockData.o}
+              color={'green'}
+            />
+          </div>
+          <div className='p-1'>
+            <Prices
+              priceName={'Close'}
+              priceValue={stockData.c}
+              color={'red'}
+            />
+          </div>
         </div>
-        <div className='p-1'>
-          <Prices priceName={'Close'} priceValue={stockData.c} color={'red'} />
+        <div className='grid grid-cols-3'>
+          <div className='p-1'>
+            <MetaData dataPointName={'High'} dataPointValue={stockData.h} />
+          </div>
+          <div className='p-1'>
+            <MetaData dataPointName={'Low'} dataPointValue={stockData.l} />
+          </div>
+          <div className='p-1'>
+            <MetaData
+              dataPointName={'Avg. Price'}
+              dataPointValue={stockData.vw}
+            />
+          </div>
+          <div className='col-span-3 p-1'>
+            <MetaData
+              dataPointName={'Volume Traded'}
+              dataPointValue={stockData.v}
+            />
+          </div>
         </div>
       </div>
-      <div className='grid grid-cols-3'>
-        <div className='p-1'>
-          <MetaData dataPointName={'High'} dataPointValue={stockData.h} />
-        </div>
-        <div className='p-1'>
-          <MetaData dataPointName={'Low'} dataPointValue={stockData.l} />
-        </div>
-        <div className='p-1'>
-          <MetaData
-            dataPointName={'Avg. Price'}
-            dataPointValue={stockData.vw}
-          />
-        </div>
-        <div className='col-span-3 p-1'>
-          <MetaData
-            dataPointName={'Volume Traded'}
-            dataPointValue={stockData.v}
-          />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
